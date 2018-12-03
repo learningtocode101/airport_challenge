2e2e820cb955357ac65587f7d6466765df2513e0
Shaneil Clarke <shaneilske@yahoo.com>
2018-12-03 21:31:21 +0100

    added test and code to instruct plane to take off

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,10 +1,14 @@
 require 'airport'
 
 describe Airport do
-  
+
   it 'instructs a plane to land' do
     plane = Plane.new
     expect(subject.instruct_to_land(plane)).to eq [plane]
+  end
+
+  it 'instructs a plane to take off and confirm it has' do
+    expect { subject.instruct_to_takeoff(Plane.new) }.equal? true
   end
 
 end
