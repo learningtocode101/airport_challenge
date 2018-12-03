3c496ddb54e6a11bc0d4a88274085b25a777236e
Shaneil Clarke <shaneilske@yahoo.com>
2018-12-03 19:30:27 +0100

    added method to instruct plane to land at airport

---
+++ lib/airport.rb
@@ -0,0 +1,8 @@
+class Airport
+
+  def instruct_to_land(plane)
+    @planes = []
+    @planes << plane
+  end
+
+end
---
+++ spec/airport_spec.rb
@@ -0,0 +1,10 @@
+require 'airport'
+
+describe Airport do
+  
+  it 'instructs a plane to land' do
+    plane = Plane.new
+    expect(subject.instruct_to_land(plane)).to eq [plane]
+  end
+
+end
